97bd1f59a309130892b0c903641a3e84eda43e61
farshid zahedi <fa.zahedi42@gmail.com>
2023-05-02 23:18:14 +0330

    add router links v1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/simple-mobile-shop" element={<Home />} />
+        <Route path="/simple-mobile-shop/about" element={<About />} />
+        <Route path="/simple-mobile-shop/contact" element={<Contact />} />
       </Routes>
     </>
   );
