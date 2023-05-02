713e5eeaf699909c8993f428305a2d9b3f7e0a53
farshid zahedi <fa.zahedi42@gmail.com>
2023-05-02 23:22:39 +0330

    add router links v1

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Header from "./Components/Header/Header";
-import Slider from "./Components/Slider/Slider";
-import Service from "./Components/Service/Service";
-import Productslider from "./Components/Productslider/Productslider";
-import Baner from "./Components/Baner/Baner";
-import Brand from "./Components/Brand/Brand";
-import Footer from "./Components/Footer/Footer";
+import Header from "./../Components/Header/Header";
+import Slider from "./../Components/Slider/Slider";
+import Service from "./../Components/Service/Service";
+import Productslider from "./../Components/Productslider/Productslider";
+import Baner from "./../Components/Baner/Baner";
+import Brand from "./../Components/Brand/Brand";
+import Footer from "./../Components/Footer/Footer";
 
 function App() {
   return (
